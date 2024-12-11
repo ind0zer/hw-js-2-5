613535cbb20b53f1062d75788bf9627fa6d00366
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-12-12 00:03:52 +0200

    1

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ let currentIndexKey = 0;
 
 const keyElement = document.getElementById("key");
 const gameStatusElement = document.getElementById("game-status");
+const newGame = document.getElementById("newGame");
+gameStatusElement.textContent = `Натисніть ${keys[currentIndexKey]}`;
 
 function updateKey() {
   keyElement.textContent = keys[currentIndexKey];
 }
 
-document.addEventListener("keydown", function (event) {
+function keyDown(event) {
   if (event.key === keys[currentIndexKey]) {
     currentIndexKey++;
 
@@ -19,6 +21,7 @@ document.addEventListener("keydown", function (event) {
     } else {
       gameStatusElement.textContent = `Вітаємо! Ви завершили гру!`;
       PNotify.success({ text: "Вітаємо! Ви завершили гру!" });
+      document.removeEventListener("keydown", keyDown);
     }
   } else {
     PNotify.error({
@@ -27,4 +30,87 @@ document.addEventListener("keydown", function (event) {
   }
 
   updateKey();
+}
+
+document.addEventListener("keydown", keyDown);
+
+function startNewGame() {
+    currentIndexKey = 0;
+    gameStatusElement.textContent = `Натисніть ${keys[currentIndexKey]}`;
+    updateKey();
+
+    document.addEventListener('keydown', keyDown);
+}
+
+newGame.addEventListener('click', startNewGame);
+
+const chartData = {
+  labels: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+  ],
+  datasets: [
+    {
+      label: "Продажі за останній місяць",
+      data: [
+        150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550,
+        600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200,
+        1250, 1300, 1350,
+      ],
+      backgroundColor: "#2196f3",
+      borderColor: "#2196f3",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const ctx = document.getElementById("salesChart");
+
+const salesChart = new Chart(ctx, {
+  type: "line",
+  data: chartData,
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+  },
 });
